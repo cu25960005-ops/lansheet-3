c25fd3321b9454cb8b5a45c0982b0bd21647e497
cu25960005-ops <cu25960005@coeruniversity.ac.in>
2025-11-12 19:54:51 +0530

    Demonstrate relational operators

---
+++ 12.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5, b = 8;
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
+    cout << (a > b) << endl;
+    cout << (a < b) << endl;
+    cout << (a >= b) << endl;
+    cout << (a <= b);
+    return 0;
+}
