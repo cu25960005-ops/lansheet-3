32451bca0ea81c1690d2c36b16b47ac573b4c81a
cu25960005-ops <cu25960005@coeruniversity.ac.in>
2025-11-12 19:56:33 +0530

    Square and cube using power operator

---
+++ 14.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    float num;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "Square = " << pow(num, 2) << endl;
+    cout << "Cube = " << pow(num, 3);
+    return 0;
+}
