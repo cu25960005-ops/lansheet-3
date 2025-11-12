24cb855f101ee4e0c07090202ae36f3e2f4d24d7
cu25960005-ops <cu25960005@coeruniversity.ac.in>
2025-11-12 19:58:01 +0530

    Demonstrate increment and decrement operators

---
+++ 16.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    cout << "x = " << x << endl;
+    cout << "x++ = " << x++ << endl;
+    cout << "After x++: " << x << endl;
+    cout << "--x = " << --x << endl;
+    return 0;
+}
