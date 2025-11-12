54e0c336d05f20475a65c8a0c9320f0a77b0d78c
cu25960005-ops <cu25960005@coeruniversity.ac.in>
2025-11-12 19:48:52 +0530

    Area of rectangle

---
+++ 4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float length, breadth;
+    cout << "Enter length and breadth: ";
+    cin >> length >> breadth;
+    cout << "Area = " << length * breadth;
+    return 0;
+}
